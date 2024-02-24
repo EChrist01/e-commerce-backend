30e79ed96cf20f68df3de3901a57a12df52feab6
EChrist01 <ethanchrist02@gmail.com>
2024-02-23 19:00:01 -0700

    final

--- models/Product.js
+++ models/Product.js
@@ -15,31 +15,29 @@ Product.init(
       primaryKey: true,
       autoIncrement: true,
     },
+
     product_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
     price: {
       type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
-        isDecimal: {
-          args: [true],
-          msg: "Price must be a decimal value!",
-        },
+        isDecimal: true,
       },
     },
+
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
       validate: {
-        isNumeric: {
-          args: [true],
-          msg: "Stock must be a numeric value!",
-        },
+        isNumeric: true,
       },
     },
+    
     category_id: {
       type: DataTypes.INTEGER,
       references: {
