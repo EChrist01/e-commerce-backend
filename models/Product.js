1aabcbb266e66a6b6bc9b56e6fc24f14a06af7e8
EChrist01 <ethanchrist02@gmail.com>
2024-02-23 18:21:18 -0700

    models inputs

--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,46 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: {
+          args: [true],
+          msg: "Price must be a decimal value!",
+        },
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: {
+          args: [true],
+          msg: "Stock must be a numeric value!",
+        },
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
+  
   {
     sequelize,
     timestamps: false,
